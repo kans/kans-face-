318b41e1dfa4a5837d40a663a5dc327e783ef57b
Matt Kaniaris <mkaniaris@gmail.com>
2012-01-04 16:31:37 +0000

    cleaned up code or something.  I don't even know, I've been up for 24 hours

--- blog/cache_control.py
+++ blog/cache_control.py
@@ -2,15 +2,21 @@
 #Copyright 2011 Matt Kaniaris
 """ handles updating some cached objects/renderings after we receive the appropriate signals"""
 
+from django.template.loader import render_to_string
 from django.db.models.signals import post_save
 from django.core.urlresolvers import reverse
 from django.core.cache import cache
 from django.dispatch import receiver
+
 from blog import utils
 from blog import models
 
+SPLASH_PAGE_KEY = 'SPLASH.KEY.HTML'
+SPLASH_SLUG = 'SPLASH.SLUG'
+
 def update_recent_posts_cache():
-  response = utils.render_recent_posts()
+  recentPosts =  models.Article.filter_live().values("slug", "title")[:5]
+  response = render_to_string('recent-posts.html', {'posts': recentPosts })
   key = reverse('blog.views.get_recent_posts')
   cache.set(key, response, 60*60*24*7)
   return response
@@ -20,9 +26,12 @@ def recent_posts_receiever(sender, **kwargs):
   """ simple receiver to update our cache for recent posts after any article
   has been saved.  We will update a bit too frequently, but how often
   do you write blog articles?"""
+  #TODO: implement caching of pages after I get sleep
 
-  if kwargs['created'] or kwargs['instance'].is_live:
+  instance = kwargs['instance']
+
+  if kwargs['created'] or instance.is_live:
     update_recent_posts_cache()
-
+    cache.set(SPLASH_SLUG, instance.slug)
 
 
--- blog/utils.py
+++ blog/utils.py
@@ -2,12 +2,3 @@
 #Copyright 2011 Matt Kaniaris
 """ some handy utils and stuff that doesn't belong anywhere really """
 
-from django.template.loader import render_to_string
-
-from blog import models
-
-def render_recent_posts():
-  recentPosts =  models.Article.filter_live().values("slug", "title")[:5]
-  return render_to_string('recent-posts.html', {'posts': recentPosts })
-
-
